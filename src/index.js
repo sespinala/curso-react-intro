4ea50b384af86a63998a02965a098e5372b32594
Sara Espinal <saraespinal1@gmail.com>
2023-05-24 21:04:35 -0500

    downgrade react 18 to react 17

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const container = document.getElementById('root');
+render(<App />, container);
